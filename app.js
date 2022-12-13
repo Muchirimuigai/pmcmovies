51396f0ae7e6e0ad7e6609561eb1c6ef6815e173
Muchirimuigai <119516066+Muchirimuigai@users.noreply.github.com>
2022-12-13 20:29:22 +0300

    js progress

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let left_btn = document.getElementsByClassName('bi-chevron-left')[0];
 let right_btn = document.getElementsByClassName('bi-chevron-right')[0];
 let cards = document.getElementsByClassName('cards')[0];
+let search = document.getElementsByClassName('search')[0];
+let search_input = document.getElementById('search_input');
+
 
 left_btn.addEventListener('click', ()=>{
     cards.scrollLeft -= 140;
@@ -21,7 +24,7 @@ fetch(json_url).then(Response => Response.json())
     card.innerHTML = ` 
     <img src="${sposter}" alt="${name}" class="poster">
     <div class="rest_card">
-        <img src="${sposter}" alt="">
+        <img src="${bposter}" alt="">
         <div class="cont">
             <h4>${name}</h4>
             <div class="sub">
@@ -39,8 +42,45 @@ fetch(json_url).then(Response => Response.json())
    document.getElementById('date').innerText = data[0].date;
 
    document.getElementById('rate').innerHTML = `
-   <span>IMDB</span><i class="bi bi-star-fill"></i> ${date[0].imdb}</p></h3>
+   <span>IMDB</span><i class="bi bi-star-fill"></i> ${data[0].imdb}</p></h3>
    `
-  
+//   search data load
+data.forEach(element =>{
+    let {name, imdb, date, sposter, genre, type, url, trailer, low, medium, high} = element;
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = url;
+    card.innerHTML = ` 
+    <img src="${sposter}" alt="">
+    <div class="con">
+        <h3>${name}</h3>
+        <p>${genre} , ${date}, <span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</p>
+    </div>
+    `
+    search.appendChild(card);
 
+});
+// search filter 
+search_input.addEventListener('keyup', ()=> {
+    let filter = search_input.value.toUpperCase();
+    let a = search.getElementsByTagName('a');
+    for(let index = 0; index < a.length; index++){
+        let b = a[index].getElementsByClassName('cont')[0];
+        let TextValue = b.textContent || b.innerText;
+        if (TextValue.toUpperCase().indexOf(filter) > -1) {
+            a[index].style.display = "flex";
+            search.style.visibility = "visible";
+            search.style.opacity = 1;
+        } else {
+            a[index].style.display = "none";
+        }
+        if (search_input.value == 0) {
+            search.style.visibility = "visible";
+            search.style.opacity = 0;  
+            
+        }
+
+    }
+
+ })
 });
